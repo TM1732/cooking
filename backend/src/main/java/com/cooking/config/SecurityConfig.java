9e8e3ae7ec3b3a8368af26c01ba7c7e7efc3d911
as-one <thomas.monnin@proton.me>
2025-06-29 18:00:50 +0200

    CORS Configuration for Http frontend

--- backend/src/main/java/com/cooking/config/SecurityConfig.java
+++ backend/src/main/java/com/cooking/config/SecurityConfig.java
@@ -125,7 +125,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
